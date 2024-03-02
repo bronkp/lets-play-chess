f20503780a6ddee75a29c6303203b155d5c3d658
rory <rorysaxton@gmail.com>
2024-03-02 15:21:03 -0700

    favicon and preview image test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from "next";
 import { Overpass } from "next/font/google";
 import "./globals.css";
-import { IconContext } from "react-icons";
-import Head from "next/head";
+
 
 const inter = Overpass({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Chess",
-  description: "Chess Board",
+  title: "lets-play-chess",
+  description: "Play chess with friends",
 };
 
 export default function RootLayout({
@@ -17,12 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" >
    
 
    <meta name="viewport" content="width=1000; " />
-      
-
+   <meta property="og:type" content="website"/>
+   <meta property="og:image" content="https://imgur.com/5gmADj3"/>
         {/* <IconContext.Provider value={{ style: { height:"3em",width:"3em" } }}> */}
 
       <body className={inter.className}>{children}</body>
