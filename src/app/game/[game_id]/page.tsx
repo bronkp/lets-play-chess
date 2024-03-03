497fa9478195a525b5946dbf973967b625800907
rory <rorysaxton@gmail.com>
2024-03-03 16:54:57 -0700

    increased icon size for mobile

--- src/app/game/[game_id]/page.tsx
+++ src/app/game/[game_id]/page.tsx
@@ -1,21 +1,14 @@
 "use client";
-import { useEffect, useState } from "react";
-import { supabase } from "../../../../supa/client";
 import OnlineBoard from "@/app/components/OnlineBoard";
-import Cord from "@/app/classes/Cord";
 import { IconContext } from "react-icons";
-import SharePopUp from "@/app/components/SharePopUp";
-type Move = {
-  x: number;
-  y: number;
-};
+
 export default function Page({ params }: { params: { game_id: string } }) {
   
 
   return (
     <>
       <div >
-        <IconContext.Provider value={{ style: { height:"3vw",width:"3vw" } }}> 
+        <IconContext.Provider value={{  style: {minWidth:"4em",minHeight:"4em", height:"3vw",width:"3vw" } }}> 
 
         {/* <button onClick={()=>sendMove()}>Send</button> */}
         <OnlineBoard
