7d706dc46e652d4e2b426b599d069b20dd192137
rory <rorysaxton@gmail.com>
2024-03-07 17:19:21 -0700

    removed graveyard functions

--- graveyard/upgradePawn.ts
+++
@@ -1,9 +0,0 @@
-//original pawn upgrade
-// function upgradePawn(piece: string) {
-//     let boardCopy = _.cloneDeep(realBoard!);
-//     boardCopy[pawnToUpgrade!.y][pawnToUpgrade!.x].piece =
-//       r[piece as keyof typeof r];
-//     setRealBoard(boardCopy);
-//     setPawnToUpgrade(null);
-//     kingVerify(boardCopy, whtKing, blkKing);
-//   }
