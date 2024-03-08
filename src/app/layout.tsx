8b8893d6b5e17bc1ff29a18cf0a6bc6d99535ed1
rory <rorysaxton@gmail.com>
2024-03-07 19:52:53 -0700

    preview scale

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,19 @@
+import ogImage from "../../public/chessGame.PNG";
 import type { Metadata } from "next";
 import { Overpass } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
-
 
 const inter = Overpass({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "lets-play-chess",
   description: "Play chess with friends",
-  openGraph:{images:["./chessGame.PNG"]}
+  openGraph: {
+    images: [
+      { url: ogImage.src, width: ogImage.width, height: ogImage.height },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -18,14 +22,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en"  prefix="og: https://ogp.me/ns#">
- 
- 
-
-        {/* <IconContext.Provider value={{ style: { height:"3em",width:"3em" } }}> */}
+    <html lang="en" prefix="og: https://ogp.me/ns#">
+      {/* <IconContext.Provider value={{ style: { height:"3em",width:"3em" } }}> */}
 
       <body className={inter.className}>{children}</body>
-        {/* </IconContext.Provider> */}
+      {/* </IconContext.Provider> */}
     </html>
   );
 }
